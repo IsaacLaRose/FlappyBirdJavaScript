5b12260978a8e614e8f9d1f27326b9ca569ff251
IsaacLaRose <ihlarose4444@gmail.com>
2026-04-20 15:47:33 -0400

    Added Scoring System, Collisions, Game State, and differing Pipe Variator

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ let Gapmaximum = 9;
 let Gapminimum = 4;
 let Gapstar = 8;
 
+let nextPipe = 0;
+
+let scoreElement = document.getElementById("score");
+let score = 0;
+let scoredPipes = new Set();
+
+
+let gameState = "waiting"; // "waiting", "playing"
+
 let pipes = [
     { col: 40, gapStart: 10, gapSize: 8 }
 ];
@@ -51,21 +60,65 @@ draw();
 document.addEventListener('keydown', (e) => { //Jumping/flapping when pressing space
   if (e.code === 'Space') {
     birdVel = -2;
+    gameState = "playing";
   }
 });
 
 setInterval(() => { //game loop
+    
+    if (gameState === "waiting") {
+        return;
+    }
     birdVel += 0.5; //gravity
     birdRow += birdVel;
+
+    nextPipe = (nextPipe + 1) % 20;
+    if (nextPipe === 0) {
+        pipes.push({
+            col: 50,
+            gapStart: Math.floor(Math.random() * (gridHeight - Gapstar - 4)) + 2,
+            gapSize: Math.floor(Math.random() * (Gapmaximum - Gapminimum) + Gapminimum)
+        });
+    }
+
+    let crashed = false;
+
     for (let pipe of pipes) {
         pipe.col -= 1;
-        if (pipe.col < 0) {
-            pipe.col = 50;
-            pipe.gapSize = Math.random() * (Gapmaximum - Gapminimum) + Gapminimum;
-            pipe.gapStart = Math.floor(Math.random() * (gridHeight - pipe.gapSize - 4)) + 2;
+
+        // ONLY check collision when bird is in same column
+        if (pipe.col === birdCol) {
+            let inGap =
+                birdRow >= pipe.gapStart &&
+                birdRow < pipe.gapStart + pipe.gapSize;
+
+            if (!inGap) {
+                crashed = true;
+            }
+            score++;
         }
     }
+
+    // global game over check
+    if (birdRow <= 0 || birdRow >= gridHeight || crashed) {
+        alert("Game Over!");
+        birdRow = 10;
+        birdVel = 0;
+        pipes = [{ col: 40, gapStart: 10, gapSize: 8 }];
+        score = 0;
+        scoredPipes = new Set();
+        gameState = "waiting";
+        return;
+    }
+        // if (pipe.col < 0) {
+        //     pipe.col = 50;
+        //     pipe.gapSize = Math.random() * (Gapmaximum - Gapminimum) + Gapminimum;
+        //     pipe.gapStart = Math.floor(Math.random() * (gridHeight - pipe.gapSize - 4)) + 2;
+        // }
+
+    scoreElement.textContent = "Score = " + score;
     draw();
+    
 }, 100)
 
 
